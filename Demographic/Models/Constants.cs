dc5d15c688e80870aa7173fe547bf405abf523b9
Dmitriy <kirilllukanov121@gmail.com>
2025-10-31 20:15:43 +0300

    Update Constants.cs

--- Demographic/Models/Constants.cs
+++ Demographic/Models/Constants.cs
@@ -5,6 +5,7 @@
         public const double INITIAL_POPULATION_SCALE = 1000.0;
         public const double BIRTH_PROBABILITY = 0.151;
         public const double FEMALE_BIRTH_PROBABILITY = 0.55;
+        public const double DEFAULT_GENDER_PROBABILITY = 0.5;
 
         public const int MIN_CHILDBEARING_AGE = 18;
         public const int MAX_CHILDBEARING_AGE = 45;
@@ -12,3 +13,4 @@
         public const double DEFAULT_DEATH_PROBABILITY = 1.0;
     }
 }
+
