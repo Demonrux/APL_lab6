91a51ad34370f5e29b4d114828550c87c310158d
Dmitriy <kirilllukanov121@gmail.com>
2025-10-31 20:09:51 +0300

    Add files via upload

---
+++ Demographic/Models/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace Demographic.Models
+{
+    public static class Constants
+    {
+        public const double INITIAL_POPULATION_SCALE = 1000.0;
+        public const double BIRTH_PROBABILITY = 0.151;
+        public const double FEMALE_BIRTH_PROBABILITY = 0.55;
+
+        public const int MIN_CHILDBEARING_AGE = 18;
+        public const int MAX_CHILDBEARING_AGE = 45;
+
+        public const double DEFAULT_DEATH_PROBABILITY = 1.0;
+    }
+}
